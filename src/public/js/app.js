1ee3b351ad12723e1c70db5112be821dacd84700
JeongJoo-Lee <jjlee1623@gmail.com>
2021-09-03 11:21:23 +0900

    모든 브라우저 메세지 전송 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,12 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
-socket.addEventListener("message", (message) =>{
+socket.addEventListener("message", (message) => {
   console.log("New message :", message.data);
 });
 
@@ -12,6 +14,11 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 3000);
+function handleSubmit(e) {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -8,28 +8,29 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"))
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // http와 ws(websocket) 프로토콜을 하나로 합치는 코드
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
+const sockets = [];
 
-wss.on("connection", (socket)=>{
-  console.log("Connected to Browser ✅")
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
 
-  socket.on("close", ()=>{
-    console.log("Disconnected from Browser ❌")
+  socket.on("close", () => {
+    console.log("Disconnected from Browser ❌");
   });
 
   socket.on("message", (message) => {
-    console.log(message.toString('utf-8'));
+    sockets.forEach((aSocket) => {
+      aSocket.send(message.toString("utf-8"));
+    });
   });
-
-  socket.send("hello!!")
-})
-
+});
 
 server.listen(3000, handleListen);
